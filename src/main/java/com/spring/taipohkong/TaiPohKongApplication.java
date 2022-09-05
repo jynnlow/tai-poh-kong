b504ff5a4bde60054a8316cad31a3bf20dd16fea
Jynn Low <lyeejiun99@gmail.com>
2022-09-05 13:52:28 +0800

    files initiated

---
+++ src/main/java/com/spring/taipohkong/TaiPohKongApplication.java
@@ -0,0 +1,13 @@
+package com.spring.taipohkong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiPohKongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaiPohKongApplication.class, args);
+	}
+
+}
